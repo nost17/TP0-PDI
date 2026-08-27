5a6d35e7de6917b70c0346b9e807f581cb0f1a40
nost17 <erick.personal16@outlook.com>
2026-08-27 13:22:30 -0300

    feat: ventana inicial

--- src/tp0_pdi/main.py
+++ src/tp0_pdi/main.py
@@ -1,2 +1,26 @@
+import tkinter as tk
+
+
+class MainApp:
+    ventana: tk.Tk
+
+    def __init__(self, ventana: tk.Tk):
+        self.ventana = ventana
+        self.ventana.title("PDI - Tkinter básico")
+        self.ventana.geometry("900x600")
+        self.crear_interfaz()
+
+    def iniciar(self) -> None:
+        self.ventana.mainloop()
+
+    def crear_interfaz(self) -> None:
+        pass
+
+
 def main() -> None:
-    print("hola mundo")
+    app = MainApp(tk.Tk())
+    app.iniciar()
+
+
+if __name__ == "__main__":
+    main()
