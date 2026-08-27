3d26aee06a0a2729bcb878eec2e90f9959d6ae4c
nost17 <erick.personal16@outlook.com>
2026-08-27 16:51:35 -0300

    feat: se agrego la funcion de abrir una imagen (en progreso)

--- src/tp0_pdi/main.py
+++ src/tp0_pdi/main.py
@@ -1,6 +1,8 @@
-import tkinter
+import numpy as np
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
 
 ESPACIO: dict[str, int | tuple[int]] = {
     "normal": 10,
@@ -9,6 +11,8 @@ ESPACIO: dict[str, int | tuple[int]] = {
     "eizq": (5, 0),
     "eder": (0, 5),
 }
+
+type ImagenArray = np.ndarray[np.dtype[np.float64]]
 
 
 class MainApp:
@@ -70,7 +74,7 @@ class MainApp:
         btn_abrir_imagen = ttk.Button(
             barra,
             text="Abrir imagen",
-            command=lambda: print("> abrir imagen"),
+            command=self.abrir_imagen,
             padding=(6, 3),
         )
         btn_aplicar_filtro = ttk.Button(
@@ -103,6 +107,59 @@ class MainApp:
         return ttk.Label(
             raiz, text=texto, anchor=tk.CENTER, borderwidth=1, relief=tk.SOLID
         )
+
+    def abrir_imagen(self):
+
+        ruta: str = filedialog.askopenfilename(
+            title="Seleccionar imagen",
+            filetypes=[("Imágenes", "*.jpg *.jpeg *.png *.bmp *.tif *.tiff")],
+        )
+
+        if not ruta:
+            return
+
+        # Pillow abre la imagen.
+        imagen_pil: Image = Image.open(ruta).convert("RGB")
+
+        # Convertimos Pillow -> NumPy.
+        #
+        # /255.0 normaliza:
+        #
+        # 0   -> 0.0
+        # 255 -> 1.0
+        #
+        self.imagen_original: ImagenArray = np.array(imagen_pil) / 255.0
+
+        # La imagen actual comienza siendo igual a la original.
+        self.imagen_actual: ImagenArray = self.imagen_original.copy()
+
+        self.mostrar_imagen(self.imagen_actual)
+
+    def mostrar_imagen(self, array_imagen: ImagenArray) -> None:
+        # Convertimos 0-1 nuevamente a 0-255.
+        imagen_uint8 = (np.clip(array_imagen, 0, 1) * 255).astype(np.uint8)
+
+        # NumPy -> Pillow
+        imagen_pil: Image = Image.fromarray(imagen_uint8)
+
+        # Acá calculamos el ancho y alto del contenedor
+        # para que la imagen ocupe su mismo tamaño
+        ancho: int = self.label_img_normal.winfo_width()
+        alto: int = self.label_img_normal.winfo_height()
+
+        if ancho <= 1 or alto <= 1:
+            ancho, alto = 600, 600
+
+        imagen_pil.thumbnail((ancho, alto), Image.Resampling.LANCZOS)
+
+        # Pillow -> Tkinter
+        foto = ImageTk.PhotoImage(imagen_pil)
+
+        # Guardamos la referencia.
+        self.label_img_normal.foto = foto
+
+        # Mostramos la imagen.
+        self.label_img_normal.config(image=foto, text="")
 
 
 def main() -> None:
